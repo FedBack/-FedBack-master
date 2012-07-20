374607d391e974e392420059b031bf78979d7ff1
Curtis Ide <curtis.ide@ieee.org>
2012-07-20 10:21:04 -0400

    added script for synchronizing dev environments.

---
+++ getFedBack.sh
@@ -0,0 +1,9 @@
+# get FedBack-specific files from development tree
+
+cp -v ~/Source/Fedback/collectd/configure ./collectd/
+cp -v ~/Source/Fedback/collectd/configure.in ./collectd/
+cp -v ~/Source/Fedback/collectd/Makefile.in ./collectd/
+cp -v ~/Source/Fedback/collectd/src/Makefile.in ./collectd/src/
+cp -v ~/Source/Fedback/collectd/src/Makefile.am ./collectd/src/
+cp -v ~/Source/Fedback/collectd/src/FedBack.c ./collectd/src/
+cp -v ~/Source/Fedback/collectd/src/FedBack.h ./collectd/src/
